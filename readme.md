88e7c8d2a375ea08b4578a7e06b9651677018f66
UNAMED666 <UNAMED666@UNAMED666-PC>
2025-06-29 10:14:58 +0700

    v2

--- readme.md
+++ readme.md
@@ -7,11 +7,10 @@
 It all started with discomfort—I couldn’t lean back comfortably in my chair without having to keep my hand on the table just to move the mouse. So I searched to see if there were any USB touchpads, but the prices were outrageous. Then I looked for Bluetooth touchpads, but only found ones bundled with mini keyboards.
 
 Eventually, I started looking for ways to build one myself and found many tutorials on converting old touchpads into external mice. BUT THEY WERE ALL SYNAPTICS. Meanwhile, the only touchpad I had was an ELAN, and fortunately, its pinout was easier to identify thanks to its simple labeling:
-
-- TV: VCC
-- TG: Ground
-- TD: Data
-- TC: Clock
+TV: VCC
+TG: Ground
+TD: Data
+TC: Clock
 
 With zero Arduino knowledge (of course idk how to use ESP32), I boldly experimented with many things until I finally got it working. Special thanks to Kristopher for the PS2-Mouse-Arduino library, T-vK for the ESP32-BLE-Mouse library, and Gaowanliang for the Synaptics-based references—which I didn’t understand at all. Without them, I would never have been able to create this masterpiece.
 
@@ -23,6 +22,8 @@ With zero Arduino knowledge (of course idk how to use ESP32), I boldly experimen
 - [x] Single-finger double tap as mouse left click (build-in from my touchpad)
 - [x] Tap and drag to enable dragging
 - [x] Physical left and right mouse button (build-in from my touchpad)
+- [x] Vertical and horizontal scroll switch using button1
+- [x] Left CTRL using button2
 
 I still don't know how to active multitouch, it just won't give any data if more than 1 finger touch it.
 
@@ -30,11 +31,13 @@ I still don't know how to active multitouch, it just won't give any data if more
 
 This program is compiled using PlatformIO. You can compile it by installing the PlatformIO plugin in VSCode. The project has already configured the `platformio.ini` file. You just need to open the project folder and click the PlatformIO `Build` button to compile.
 
-You need to modify the 48th and 49th lines in the `src/main.cpp` file, change `#define MDATA` and `#define MCLK` to your actual pins.
+You need to modify th lines in the `src/main.cpp` file, change to your actual pins.
 
 ```cpp
 #define MDATA 5
-#define MCLK 23
+#define MCLK 4
+const int buttonPin1 = 22;
+const int buttonPin2 = 23;
 ```
 
 ## Contribution
@@ -45,7 +48,7 @@ You're welcome to submit issues and pull requests to improve the project.
 
 [@Gaowanliang's ESP32-BLE-Touchpad project](https://github.com/gaowanliang/ESP32-BLE-Touchpad)
 
-[@T-vK's ESP32-BLE-Mouse project](https://github.com/T-vK/ESP32-BLE-Mouse)
+[@peter-pakanun's ESP32-BLE-Combo project](https://github.com/peter-pakanun/ESP32-BLE-Combo)
 
 [@Kristopher's PS2-Mouse-Arduino project](https://github.com/kristopher/PS2-Mouse-Arduino)
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,25 @@
 #include "PS2Mouse.h"
 #include <Arduino.h>
 #include <stdint.h>
-#include <BleMouse.h>
-BleMouse bleMouse;
+#include <BleCombo.h>
+
+BleCombo bleCombo;
 
 #define MDATA 5
-#define MCLK 23
-#define SENSITIVITY 5
+#define MCLK 4
+#define SCROLL_SENSITIVITY 0.25f
+
+float accumScrollX = 0;
+float accumScrollY = 0;
+float scrollFactor = 0.25; 
+
 const int LED_PIN = 2;
+const int buttonPin1 = 22;
+const int buttonPin2 = 23;
+
+bool lastState1 = HIGH;
+bool lastState2 = HIGH;
+bool toggle1 = true;
 
 PS2Mouse mouse_one(MCLK, MDATA, STREAM);
 
@@ -19,13 +31,16 @@ void setup() {
   Serial.begin(115200);
   ledcSetup(0, 5000, 8);
   ledcAttachPin(LED_PIN, 0);
+  pinMode(buttonPin1, INPUT_PULLUP);
+  pinMode(buttonPin2, INPUT_PULLUP);
   
   mouse_one.initialize();
   mouse_one.set_stream_mode();
   mouse_one.set_scaling_1_1();
 
-  Serial.println("BLE Mouse started");
-  bleMouse.begin();
+  Serial.println("BLE started");
+  bleCombo.begin();
+  Serial.println("BLE started");
 }
 
 unsigned long lastUpdate = 0;
@@ -34,7 +49,17 @@ int fadeAmount = 5;
 
 void updateLED() {
   unsigned long now = millis();
-  if (now - lastUpdate > 100) {
+  if (now - lastUpdate > 40) {
+    brightness += fadeAmount;
+    if (brightness <= 0 || brightness >= 80) fadeAmount = -fadeAmount;
+    ledcWrite(0, brightness);
+    lastUpdate = now;
+  }
+}
+
+void updateLED2() {
+  unsigned long now = millis();
+  if (now - lastUpdate > 10) {
     brightness += fadeAmount;
     if (brightness <= 0 || brightness >= 80) fadeAmount = -fadeAmount;
     ledcWrite(0, brightness);
@@ -43,7 +68,9 @@ void updateLED() {
 }
 
 void loop() {
-  updateLED();
+  bool currentState1 = digitalRead(buttonPin1);
+  bool currentState2 = digitalRead(buttonPin2);
+  
   int16_t data[2];
   mouse_one.report(data);
 
@@ -56,33 +83,136 @@ void loop() {
     Serial.print(dx); Serial.print(",");
     Serial.println(dy);
   }
+if (lastState1 == HIGH && currentState1 == LOW) {
+  toggle1 = !toggle1;
+  Serial.print("Toggle aktif: ");
+  Serial.println(toggle1);
+}
 
-  if (bleMouse.isConnected()) {
+  if (bleCombo.isConnected()) {
     // Deteksi tombol dari status byte
     bool isLeft = dw & 0x01;
     bool isRight = dw & 0x02;
 
+    // Mouse Kiri
+    // Jika tombol kiri ditekan dan belum ditekan sebelumnya
     if (isLeft && !leftPressed) {
-      bleMouse.press(MOUSE_LEFT);
+      if (currentState2 == LOW) {
+           bleCombo.press((MouseButton)MOUSE_BACK);     
+      }
+      else {
+         if (toggle1==1){ //scroll mode
+          
+            bleCombo.press((MouseButton)MOUSE_LEFT);     
+        }
+      }
       leftPressed = true;
-    } else if (!isLeft && leftPressed) {
-      bleMouse.release(MOUSE_LEFT);
+    } 
+    
+    else if (!isLeft && leftPressed) {
+      bleCombo.release((MouseButton)MOUSE_BACK);
+      bleCombo.release((MouseButton)MOUSE_LEFT);
       leftPressed = false;
     }
+  
 
+    // Mouse Kanan
+    // Jika tombol kanan ditekan dan belum ditekan sebelumnya
     if (isRight && !rightPressed) {
-      bleMouse.press(MOUSE_RIGHT);
+      if (currentState2 == LOW) {       
+          bleCombo.press((MouseButton)MOUSE_FORWARD);
+      }
+      else {
+        if (toggle1==1){ //scroll mode
+           bleCombo.press((MouseButton)MOUSE_RIGHT);
+        }
+      }
       rightPressed = true;
-    } else if (!isRight && rightPressed) {
-      bleMouse.release(MOUSE_RIGHT);
+    } 
+    
+    else if (!isRight && rightPressed) {
+      bleCombo.release((MouseButton)MOUSE_FORWARD);
+      bleCombo.release((MouseButton)MOUSE_RIGHT);
       rightPressed = false;
     }
 
-    // Gerakan
-    if (dx != 0 || dy != 0) {
-      bleMouse.move(dx, dy);
-    }
-  }
 
+    
+    //mouseleft scroll
+    if (isLeft) {       
+       if (toggle1==0){ //scroll mode         
+           bleCombo.move(0,0,-1,0);
+           delay(50);
+        }
+    }
+    if (isRight) {       
+       if (toggle1==0){ //scroll mode         
+           bleCombo.move(0,0,1,0);
+           delay(50);
+        }
+    }
+
+    //LED
+    if ((toggle1) && (currentState2 == HIGH)) {
+      updateLED(); // Mode gerak - berkedip
+    } else if ((!toggle1) && (currentState2 == HIGH)) {
+      ledcWrite(0, 80); // Mode scroll - nyala tetap
+    }
+    else {
+      ledcWrite(0, 0); // Matikan LED
+    }
+
+    //Tombol1
+    if (dx != 0 || dy != 0) {
+    if (toggle1) {
+
+      bool xOverflow = dw & 0x40;
+      bool yOverflow = dw & 0x80;
+
+      if (xOverflow || yOverflow) {
+       dx = 0;
+        dy = 0;
+      } else {
+        dx = constrain(dx, -30, 30);
+        dy = constrain(dy, -30, 30);
+        bleCombo.move(-dy * 4, -dx * 4);
+      }
+    } 
+    else {
+
+        accumScrollX += dx * SCROLL_SENSITIVITY;
+        accumScrollY += dy * SCROLL_SENSITIVITY;
+
+        int scrollX = -(int)accumScrollX;
+        int scrollY = -(int)accumScrollY;
+
+       // Kirim scroll horizontal jika ada
+        if (dy != 0) {
+           bleCombo.move(0, 0, 0, dy);
+            accumScrollY -= scrollY;
+           delay(5);
+        }
+
+        // Kirim scroll vertikal jika ada
+        if (dx != 0) {
+          bleCombo.move(0, 0, dx, 0);
+          accumScrollX -= scrollX;          
+        }       
+    }
+    }
+    //Tombol2
+    if (currentState2 == LOW) {
+         bleCombo.press((uint8_t)KEY_LEFT_CTRL);
+         Serial.print("CTRL ");
+      } 
+    else if (!currentState2 == LOW) {
+         bleCombo.release((uint8_t)KEY_LEFT_CTRL);
+      } 
+  }
+  else{
+    updateLED2(); // Jika tidak terhubung, LED tetap berkedip
+  }
+  lastState1 = currentState1;
+  
   delay(10);
 }
